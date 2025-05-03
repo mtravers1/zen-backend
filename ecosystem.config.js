7d1e3b8b51f7c1410a6ae2bba35991a2fb127659
Merida <cmerida@bluepeople.com>
2025-05-02 19:41:07 -0600

    arx-007 - ops: Fixes pm2 log path parameters (squash)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,19 +4,28 @@ module.exports = {
             name: "dev",
             script: "./index.js",
             watch: true,
-            node_args: "--env-file=.env"
+            node_args: "--env-file=.env",
+            error_file: "/var/log/zentavos/api-error.log",
+            out_file: "/var/log/zentavos/api-out.log",
+            log_date_format: "YYYY-MM-DD HH:mm Z",
         },
         {
             name: "testing",
             script: "./index.js",
             watch: true,
-            node_args: "--env-file=.env"
+            node_args: "--env-file=.env",
+            error_file: "/var/log/zentavos/api-error.log",
+            out_file: "/var/log/zentavos/api-out.log",
+            log_date_format: "YYYY-MM-DD HH:mm Z",
         },
         {
             name: "uat",
             script: "./index.js",
             watch: true,
-            node_args: "--env-file=.env"
+            node_args: "--env-file=.env",
+            error_file: "/var/log/zentavos/api-error.log",
+            out_file: "/var/log/zentavos/api-out.log",
+            log_date_format: "YYYY-MM-DD HH:mm Z",
         },
         {
             name: "prod",
