3aff1b200cbb96181620d8d9bb23db8fc5b822ae
Merida <cmerida@bluepeople.com>
2025-06-04 12:06:48 -0600

    arx-007 - ops: Added prod workflow file to all branches for a better visibility

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,11 @@
+// IMPORTANT: The "name" attribute on each item in "apps" is used to identify the environment in PM2
+// and needs to match the DEPLOYMENT_NAME environment variable set in the .github/workflow/zentavos.<environment>.yml
+// file for the respective environment (dev, uat, prod).
+// "uat" is used for the staging environment.
 module.exports = {
     apps: [
         {
             name: "dev",
-            script: "./index.js",
-            watch: true,
-            node_args: "--env-file=.env",
-            error_file: "/var/log/zentavos/api-error.log",
-            out_file: "/var/log/zentavos/api-out.log",
-            log_date_format: "YYYY-MM-DD HH:mm Z",
-        },
-        {
-            name: "testing",
             script: "./index.js",
             watch: true,
             node_args: "--env-file=.env",
