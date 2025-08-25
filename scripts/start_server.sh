61244aca91f7a8c479f2ee7ec1f650c7e00616e2
Felipefn15 <felipefrancanogueira@gmail.com>
2025-08-25 19:44:03 -0300

    Add test runner and server start scripts
    - Introduced `run-tests.sh` for managing and executing various test types with options for coverage, watch mode, and verbose output.
    - Added `start_server.sh` to streamline server deployment using PM2, including stopping, deleting, and starting the server with specified configurations.
    - Both scripts enhance the development workflow by automating testing and deployment processes.

---
+++ scripts/start_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DEPLOYMENT_ENV=dev
+pm2 stop --silent ${DEPLOYMENT_ENV}
+pm2 delete --silent ${DEPLOYMENT_ENV}
+npm install
+pm2 start ecosystem.config.js \
+    --only ${DEPLOYMENT_ENV} \
+    --max-memory-restart 1G \
+    --log-date-format 'YYYY-MM-DD HH:mm Z'
+
+pm2 save
