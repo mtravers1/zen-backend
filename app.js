b74bd5e5d8a85c63c89bea222433cefc59357ef5
Chris Stevens <it@antemodal.com>
2025-11-04 21:18:48 -0600

    fix(ci): load .env file at runtime
    - Use dotenv directly in app.js to load the .env file.
    - This ensures that the .env file is loaded at runtime on the server,
      and not at build time on the CI runner.
    - This fixes the "ENOENT: no such file or directory" error in the CI.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
-import "./config/env.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
