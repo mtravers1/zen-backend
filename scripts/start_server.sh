081ff9b87f034dccb0050ec6e18eb790f3bc23fe
Chris Stevens <it@antemodal.com>
2025-11-20 21:26:52 -0600

    chore(debug): Add diagnostic step to start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -15,8 +15,11 @@ fi
 node --version
 npm --version
 
-echo "--- DEBUG: Starting PM2 ---"
-# Use pm2 reload for zero-downtime deployments and --no-daemon for loud logging
+echo '--- DEBUG: Starting PM2 ---'
+
+echo "--- DIAGNOSTIC: Attempting to run the app directly to expose startup errors ---"
+node index.js
+
 pm2 startOrReload ecosystem.config.cjs \
     --only ${DEPLOYMENT_ENV} \
     --name ${DEPLOYMENT_ENV} \
