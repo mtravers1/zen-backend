4f6db4fd03760a841cbaf52cc78cc4e5ce4404d2
Chris Stevens <it@antemodal.com>
2025-12-23 00:39:36 -0600

    Use instrument.mjs for Sentry instrumentation

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,25 +1,11 @@
 // ecosystem.config.cjs
-const { execSync } = require('child_process');
-
-// 1. Force run version.sh (Since PM2 ignores package.json scripts)
-try {
-  console.log("🔄 Running version.sh...");
-  execSync('./scripts/version.sh', { stdio: 'inherit' });
-} catch (err) {
-  console.error("⚠️  Failed to run version.sh. Proceeding...");
-}
-
-// IMPORTANT: The "name" attribute on each item in "apps" is used to identify the environment in PM2
-// and needs to match the DEPLOYMENT_NAME environment variable set in the .github/workflow/zentavos.<environment>.yml
-// file for the respective environment (dev, uat, prod).
-// "uat" is used for the staging environment.
 module.exports = {
   apps: [
     {
       name: "local",
       script: "./index.js",
-      node_args: "--import instrument.js --enable-source-maps",
       env: {
+        NODE_OPTIONS: "--import ./instrument.mjs --enable-source-maps",
         DOTENV_CONFIG_PATH: "./.env",
         PLAID_ENV: "sandbox",
       },
@@ -31,8 +17,8 @@ module.exports = {
       name: "dev",
       script: "./index.js",
       watch: true,
-      node_args: "--import ./instrument.js --enable-source-maps",
       env: {
+        NODE_OPTIONS: "--import ./instrument.mjs --enable-source-maps",
         PLAID_ENV: "development",
       },
       error_file: "/var/log/zentavos/api-error.log",
@@ -43,8 +29,8 @@ module.exports = {
       name: "staging",
       script: "./index.js",
       watch: false,
-      node_args: "--import ./instrument.js --enable-source-maps",
       env: {
+        NODE_OPTIONS: "--import ./instrument.mjs --enable-source-maps",
         PLAID_ENV: "production",
       },
       error_file: "/var/log/zentavos/api-error.log",
@@ -55,8 +41,8 @@ module.exports = {
       name: "prod",
       script: "./index.js",
       watch: false,
-      node_args: "--import ./instrument.js --enable-source-maps",
       env: {
+        NODE_OPTIONS: "--import ./instrument.mjs --enable-source-maps",
         PLAID_ENV: "production",
       },
       error_file: "/var/log/zentavos/api-error.log",
