da27cf867ce902cb5bc733cd6cb452c1cb5c1623
Chris Stevens <it@antemodal.com>
2025-12-23 02:51:52 -0600

    Use absolute path for Sentry instrumentation

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,3 +1,5 @@
+const path = require('path');
+
 // ecosystem.config.cjs
 module.exports = {
   apps: [
@@ -5,9 +7,7 @@ module.exports = {
       name: "local",
       script: "./index.js",
       env: {
-        NODE_OPTIONS: "--import ./instrument.mjs --enable-source-maps",
-        
-        DOTENV_CONFIG_PATH: "./.env",
+NODE_OPTIONS: `--import ${path.join(__dirname, 'instrument.mjs')} --enable-source-maps`,
         PLAID_ENV: "sandbox",
       },
       error_file: "/var/log/zentavos/api-error.log",
@@ -19,7 +19,7 @@ module.exports = {
       script: "./index.js",
       watch: true,
       env: {
-        NODE_OPTIONS: "--import ./instrument.mjs --enable-source-maps",
+        NODE_OPTIONS: `--import ${path.join(__dirname, 'instrument.mjs')} --enable-source-maps`,
         
         PLAID_ENV: "development",
       },
@@ -32,7 +32,7 @@ module.exports = {
       script: "./index.js",
       watch: false,
       env: {
-        NODE_OPTIONS: "--import ./instrument.mjs --enable-source-maps",
+        NODE_OPTIONS: `--import ${path.join(__dirname, 'instrument.mjs')} --enable-source-maps`,
         
         PLAID_ENV: "production",
       },
@@ -45,7 +45,7 @@ module.exports = {
       script: "./index.js",
       watch: false,
       env: {
-        NODE_OPTIONS: "--import ./instrument.mjs --enable-source-maps",
+        NODE_OPTIONS: `--import ${path.join(__dirname, 'instrument.mjs')} --enable-source-maps`,
         
         PLAID_ENV: "production",
       },
