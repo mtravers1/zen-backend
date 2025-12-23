8f97124f4eabefedcc91e283d2aeda1f6e4aacc6
Chris Stevens <chris.stevens@antemodal.com>
2025-12-22 20:37:04 -0600

    Fix version

--- instrument.js
+++ instrument.js
@@ -3,5 +3,6 @@ import * as Sentry from "@sentry/node";
 Sentry.init({
   dsn: "https://3b0788b88b1203668fd993c64bdebc8f@o4510568686092288.ingest.us.sentry.io/4510580113145856",
   sendDefaultPii: true,
+  release: process.env.SENTRY_RELEASE,
   tracesSampleRate: 1.0,
 });
