5a8b08247360ea0bb660e8fa631419b8e9ed2d01
Chris Stevens <it@antemodal.com>
2025-12-23 01:14:49 -0600

    Attempt manual Sentry instrumentation in app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+import * as Sentry from "@sentry/node";
+
+Sentry.init({
+  dsn: "https://3b0788b88b1203668fd993c64bdebc8f@o4510568686092288.ingest.us.sentry.io/4510580113145856",
+  sendDefaultPii: true,
+  release: process.env.SENTRY_RELEASE,
+  tracesSampleRate: 1.0,
+  registerEsmLoaderHooks: false,
+});
 
 import express from "express";
 import cookieParser from "cookie-parser";
@@ -17,8 +26,7 @@ import {
 } from "./middlewares/structuredLogging.js";
 import routeValidationMiddleware from "./middlewares/routeValidation.js";
 import connectDB from "./database/database.js";
-import router from "./routes/index.js";
-import * as Sentry from "@sentry/node";
+
 
 export async function createApp() {
 
