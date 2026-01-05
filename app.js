d59529ea67b7f193825eff07f42a0f0253f3659b
Chris Stevens <it@antemodal.com>
2026-01-05 10:32:30 -0600

    Fix: Correct Sentry initialization order in app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import * as Sentry from "@sentry/node";
+
+import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
@@ -17,6 +20,18 @@ import {
 import routeValidationMiddleware from "./middlewares/routeValidation.js";
 import connectDB from "./database/database.js";
 import router from "./routes/index.js";
+
+Sentry.init({
+  dsn: "https://3b0788b88b1203668fd993c64bdebc8f@o4510568686092288.ingest.us.sentry.io/4510583026024448",
+  sendDefaultPii: true,
+  integrations: [
+    // send console.log, console.warn, and console.error calls as logs to Sentry
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+  tracesSampleRate: 1.0,
+});
 
 
 export async function createApp() {
