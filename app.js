d1cec3d2e911cefa5b5c600aa6f9b30791b348e4
Chris Stevens <it@antemodal.com>
2026-01-05 10:24:45 -0600

    Refactor: Adjustments to Sentry and middleware configurations.

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-import * as Sentry from "@sentry/node";
-
-import "dotenv/config";
-Sentry.init({
-  dsn: "https://3b0788b88b1203668fd993c64bdebc8f@o4510568686092288.ingest.us.sentry.io/4510583026024448",
-  sendDefaultPii: true,
-  integrations: [
-    // send console.log, console.warn, and console.error calls as logs to Sentry
-    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
-  ],
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
-  tracesSampleRate: 1.0,
-});
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
