7c07eabdf90f3e7586d0a566215ebcbc0be702af
Chris Stevens <it@antemodal.com>
2026-01-05 11:36:53 -0600

    Revert: Revert Sentry and logging configurations to a previous state

--- app.js
+++ app.js
@@ -22,15 +22,11 @@ import connectDB from "./database/database.js";
 import router from "./routes/index.js";
 
 Sentry.init({
-  dsn: "https://3b0788b88b1203668fd993c64bdebc8f@o4510568686092288.ingest.us.sentry.io/4510583026024448",
+  dsn: "https://3b0788b88b1203668fd993c64bdebc8f@o4510568686092288.ingest.us.sentry.io/4510580113145856",
   sendDefaultPii: true,
-  integrations: [
-    // send console.log, console.warn, and console.error calls as logs to Sentry
-    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
-  ],
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+  release: process.env.SENTRY_RELEASE,
   tracesSampleRate: 1.0,
+  registerEsmLoaderHooks: false,
 });
 
 
