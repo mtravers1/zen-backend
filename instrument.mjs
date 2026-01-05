c36918a4b135ea9829cb8c0e5c55f7d0acfa8a1f
Chris Stevens <chris.stevens@antemodal.com>
2026-01-05 11:58:27 -0600

    Sentry update

--- instrument.mjs
+++ instrument.mjs
@@ -2,7 +2,7 @@ import "dotenv/config";
 import * as Sentry from "@sentry/node";
 
 Sentry.init({
-  dsn: "https://3b0788b88b1203668fd993c64bdebc8f@o4510568686092288.ingest.us.sentry.io/4510583026024448",
+  dsn: "https://3b0788b88b1203668fd993c64bdebc8f@o4510568686092288.ingest.us.sentry.io/4510580113145856",
   sendDefaultPii: true,
 
   tracesSampleRate: 1.0,
